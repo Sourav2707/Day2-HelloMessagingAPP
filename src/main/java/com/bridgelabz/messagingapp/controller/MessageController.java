212d835a1b5e215930d1795fbf2bd76ccea312c8
Sourav2707 <smartsourav33@gmail.com>
2023-06-08 20:46:57 +0530

    UC2 Use GET Request Method and pass name as query parameter

--- src/main/java/com/bridgelabz/messagingapp/controller/MessageController.java
+++ src/main/java/com/bridgelabz/messagingapp/controller/MessageController.java
@@ -1,14 +1,20 @@
 package com.bridgelabz.messagingapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/hello")
 public class MessageController {
 
     //http://localhost:8080/hello
-    @GetMapping("/hello")
+    @GetMapping ("")
     public String helloMessage() {
         return "Hello from BridgeLabz";
+    }
+
+    //localhost:8080/hello/query?name=Sourav
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+    public String sayHelloMessage(@RequestParam(value = "name") String name) {
+        return "Hello "+name+" from BridgeLabz";
     }
 }
